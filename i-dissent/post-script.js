b079a44814199be50adb5cc8612cba0e8f5942ed
RyanLilienthal <rl@ryanlilienthal.com>
2022-04-30 18:49:40 -0400

    comment out poem

--- i-dissent/post-script.js
+++ i-dissent/post-script.js
@@ -1,22 +1,54 @@
+// I DESCEND
+
+// I descend into the shadow of my innerHTML
+// Cascading sheets illuminate privacy's penumbra 
+
 let privacy = document.getElementById("protected-class");
+
+// Which class?
+// How to id? To id one element might exclude all others
+// Amendments alter a body's constitution
+// But to what extent?
+// Who finds sanctuary?
 
 let count = 0;
 
 window.addEventListener("click", theBillofRights)
 
+// To enumerate or not, 
+// what
+
 function theBillofRights() {
 
+// extends
+// And beyond that
+// Who can
+
 	count++
+
+// Who can 
+
 	console.log(count)
+
+// What
 
 	if (privacy.innerHTML === "cast into darkness fundamental rights illuminated in the Constitution's penumbra") {
 
+// Be warned if
+
         privacy.innerHTML = "THE SHADOW DOCKET";
 
+// And then
 
 		privacy.classList.add("impunity");
 
+// Once elucidating protection, three new fates shatter protections
+
     } else if (count >=3) {
+
+// Trump justice
+// Justices 
+// Blind justice 
 
 		privacy.innerHTML = "cast into darkness fundamental rights illuminated in the Constitution's penumbra";
 		count = 0;
@@ -33,14 +65,17 @@ function theBillofRights() {
 	}
 }
 
-// Where does privacy find protection?
-// The Court waits and Wades into seen
-// Amendments like Peter Pan
-// Reconnecting with one's ever-present silhouette
+// Unprecedented challenges precedents
+// Unprecedent stare decisis
+// To stare, but not to see 
+// To bury what was once found
+
+// Res judicata 
+// Erased
+
+// The Court Wades like Peter Pan chasing shadows
+// To rejoin one's ever-present but elusive silhouette
 // One's essential self
-// Row 
-// Roe 
-// Row Home
-// Home Row
-// The treasured source tucked away at Home
-// Illuminated on Command 
+// The Court Rows
+// It rocks Roe 
+// The innerHTML finds sanctuary at Home 
